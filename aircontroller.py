c8219dd704cd46b9bf43b729f92a2e25b4d64c30
dev-moo <mail4andrewm@gmail.com>
2016-07-19 16:50:20 +0800

    Update aircontroller.py

--- aircontroller.py
+++ aircontroller.py
@@ -4,13 +4,18 @@ import socket
 import sys
 import json
 import time
+import threading
 
 
 sys.path.insert(0, '/var/scripts/aircontroller')
 import acinterface as ac
 import sleeptimer
 
+#Globals
 stimer = sleeptimer.Sleep_Timer()	
+statusData = None
+repeat_timer = None
+update_interval = 1200
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -20,7 +25,6 @@ server_address = ('192.168.1.6', 10000)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 
-lastData = None
 
 def sleep_ac(mins):
 
@@ -47,6 +51,8 @@ def get_schedule():
 	
 def getsettings():
 
+	global statusData
+	
 	aircon = ac.AC_Unit()
 
 	if aircon is None:
@@ -63,7 +69,7 @@ def getsettings():
 	
 	aircon = None
 	
-	lastData = json.dumps(d)
+	statusData = json.dumps(d)
 	
 	return json.dumps(d)
 	
@@ -100,59 +106,86 @@ def setsetting(cmd):
 
 
 
+
+#Call getsettings at a regular interval to keep status up to date and improve performance	
+def updatesettings():
+
+	global repeat_timer
+	
+	if statusData:
+		t = json.loads(statusData)
+		
+		if time.time() - t['TimeStamp'] >= update_interval:
+			getsettings()
+
+	repeat_timer = None
+	repeat_timer = threading.Timer(update_interval, updatesettings)
+	repeat_timer.setName("update_settings_thread")
+	repeat_timer.start()
+
+
+updatesettings()
+
+
+
 if __name__ == "__main__":
 
+	try:
+		
+		while True:
+			print >>sys.stderr, '\nwaiting to receive message'
+			data, address = sock.recvfrom(4096)
 	
-		
-	while True:
-		print >>sys.stderr, '\nwaiting to receive message'
-		data, address = sock.recvfrom(4096)
-
-		print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
-		print >>sys.stderr, data
-
-		
-		if data:
-			try:
-				command = json.loads(data)
-				
-				print command
-				
-				if command['Operation'] == "GET":
-				
-					if command['Type'] == "Settings":
-						data = getsettings()
-						if data is not False:
-							sent = sock.sendto(data, address)
-						else:
-							sent = sock.sendto("failed", address)
-					
-					elif command['Type'] == "Schedule":
-						
-						print "Schedule request received"
-						
-						data = get_schedule()
-						
-						#print "sending" + data + " to " + address
-						
-						sent = sock.sendto(data, address)
-						
-						#if data is not False:
-						#	sent = sock.sendto(data, address)
-						#else:
-							#sent = sock.sendto("failed", address)						
-						
-					print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
-				
-				elif command['Operation'] == "SET":
-					setsetting(command)
-				else:
-					print "Unexpected Data:"
-					print data
+			print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
+			print >>sys.stderr, data
+	
 			
-			except:
-				print "Not JSON"
-				print data
-				print ""
-
+			if data:
+				try:
+					command = json.loads(data)
+					
+					print command
+					
+					if command['Operation'] == "GET":
+					
+						if command['Type'] == "Settings":
+							#data = getsettings()
+							data = statusData
+							if data is not False:
+								sent = sock.sendto(data, address)
+							else:
+								sent = sock.sendto("failed", address)
+								
+							getsettings()
+						
+						elif command['Type'] == "Schedule":
+							
+							print "Schedule request received"
+							
+							data = get_schedule()
+							
+							#print "sending" + data + " to " + address
+							
+							sent = sock.sendto(data, address)
+							
+							#if data is not False:
+							#	sent = sock.sendto(data, address)
+							#else:
+								#sent = sock.sendto("failed", address)						
+							
+						print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
+					
+					elif command['Operation'] == "SET":
+						setsetting(command)
+					else:
+						print "Unexpected Data:"
+						print data
+				
+				except:
+					print "Not JSON"
+					print data
+					print ""
+					
+	finally:
+		repeat_timer.cancel()
 			
